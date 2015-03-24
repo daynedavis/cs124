8cbbe9115ef6b9d3c042b5b87ad790cd290e9ba4
Dayne Davis <daynedavis5@gmail.com>
2015-03-24 12:36:33 -0400

    recursive method for pset5

--- pset5.py
+++ pset5.py
@@ -3,21 +3,19 @@ breaks = map(int, raw_input().split())
 num_breaks = first_line[0]
 length = first_line[1]
 
-def splitter(length, breaks):
+def splitter(l, breaks):
 	if len(breaks) == 0:
-		return [0,[]]
+		return 0
 	min_cut_order = []
 	min_time = 1000000000
-	for k in breaks:
-		left_break = [ x for x in breaks if x < k]
+	for index, k in enumerate(breaks):
+		left_break = breaks[:index]
 		right_break = [ x - k for x in breaks if x > k]
 		left_time = splitter(k, left_break)
-		right_time = splitter(length-k, right_break)
-		time = length + left_time[0] + right_time[0]
-		cut_order = [k] + left_time[1] + [x + k for x in right_time[1]]
+		right_time = splitter(l-k, right_break)
+		time = l + left_time + right_time
 		if time < min_time:
 			min_time = time
-			min_cut_order = cut_order
-	return (min_time, min_cut_order)
+	return min_time
 
-print splitter(length, breaks)[0]
+print splitter(length, breaks)
